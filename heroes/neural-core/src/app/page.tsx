7a7007902fc2e0c18b16b69c3311593af1a6a5c0
Priyansu Das <priyansudas273@gmail.com>
2026-08-27 21:38:45 +0530

    Revert "Implement Component #2: Fluid Particle Field with velocity simulation and vortex forces"
    This reverts commit 28d956a0dddb4e18d975085f62d5c03031c66334.

--- heroes/neural-core/src/app/page.tsx
+++ heroes/neural-core/src/app/page.tsx
@@ -1,16 +1,14 @@
-import { FluidParticleCanvas } from '@/components/FluidParticleField/FluidParticleCanvas';
+import { NeuralCoreCanvas } from '@/components/NeuralCore/NeuralCoreCanvas';
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-[#030308]">
-      <FluidParticleCanvas
-        particleCount={25000}
-        forceRadius={3.0}
-        forceStrength={2.5}
-        vortexStrength={1.8}
-        damping={0.96}
-        turbulence={0.4}
-        color="#00F0FF"
+      <NeuralCoreCanvas
+        nodeCount={3500}
+        interactionRadius={2.5}
+        attractionStrength={0.25}
+        primaryColor="#00F0FF"
+        secondaryColor="#A040FF"
         backgroundColor="#030308"
       />
     </main>
